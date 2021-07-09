81ce31218c94b5762479c006a67de8134fad40d3
umair7007 <ahmedumair7007@gmail.com>
2021-07-10 04:12:12 +0500

    level design

---
+++ shopscene/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed=2;
+
+    private Animator animator;
+    float dirx;
+    float diry;
+    public Rigidbody2D rb;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        dirx = Input.GetAxis("Horizontal");
+        diry = Input.GetAxis("Vertical");
+     
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity=new Vector2(dirx*speed,diry*speed);
+    }
+    
+}
+
